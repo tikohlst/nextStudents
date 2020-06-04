3facb3f69387c2841b25ae5beb30ae369a2144cb
Benedict Zendel <benedict.zendel@student.hs-rm.de>
2020-06-04 14:33:14 +0200

    Add placeholder text functionality to bioTextView

--- nextDoor/ProfileViewController.swift
+++ nextDoor/ProfileViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, UITextViewDelegate {
 
     @IBOutlet weak var firstNameText: UITextField!
     @IBOutlet weak var lastNameText: UITextField!
@@ -17,11 +17,60 @@ class ProfileViewController: UIViewController {
     @IBOutlet weak var radiusText: UITextField!
     @IBOutlet weak var bioTextView: UITextView!
     @IBOutlet weak var saveButton: UIButton!
+    let placeholderText = "Erzähl was über dich..."
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        bioTextView.delegate = self
+        bioTextView.text = placeholderText
+        bioTextView.textColor = UIColor.lightGray
+        
         // Do any additional setup after loading the view.
+    }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        // Combine the textView text and the replacement text to
+        // create the updated text string
+        let currentText:String = textView.text
+        let updatedText = (currentText as NSString).replacingCharacters(in: range, with: text)
+        
+        // If updated text view will be empty, add the placeholder
+        // and set the cursor to the beginning of the text view
+        if updatedText.isEmpty {
+            
+            textView.text = placeholderText
+            textView.textColor = UIColor.lightGray
+            
+            textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+        }
+            
+            // Else if the text view's placeholder is showing and the
+            // length of the replacement string is greater than 0, set
+            // the text color to black then set its text to the
+            // replacement string
+        else if textView.textColor == UIColor.lightGray && !text.isEmpty {
+            textView.textColor = UIColor.black
+            textView.text = text
+        }
+            
+            // For every other case, the text should change with the usual
+            // behavior...
+        else {
+            return true
+        }
+        
+        // ...otherwise return false since the updates have already
+        // been made
+        return false
+    }
+    
+    func textViewDidChangeSelection(_ textView: UITextView) {
+        if self.view.window != nil {
+            if textView.textColor == UIColor.lightGray {
+                textView.selectedTextRange = textView.textRange(from: textView.beginningOfDocument, to: textView.beginningOfDocument)
+            }
+        }
     }
     
 
