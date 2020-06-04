9354e4cbc0d44464c59a1ab15552876efe666ae4
Benedict Zendel <benedict.zendel@student.hs-rm.de>
2020-06-04 10:28:26 +0200

    Add profile and password view controller

---
+++ nextDoor/PasswordViewController.swift
@@ -0,0 +1,30 @@
+//
+//  PasswordViewController.swift
+//  nextDoor
+//
+//  Created by Benedict Zendel on 04.06.20.
+//  Copyright © 2020 Tim Kohlstadt. All rights reserved.
+//
+
+import UIKit
+
+class PasswordViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
