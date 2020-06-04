1ea5a2c69387724859711160e6ab708092d0122a
Benedict Zendel <benedict.zendel@student.hs-rm.de>
2020-06-04 11:27:47 +0200

    Begin touchSave method

--- nextDoor/PasswordViewController.swift
+++ nextDoor/PasswordViewController.swift
@@ -9,12 +9,21 @@
 import UIKit
 
 class PasswordViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var newPasswordText: UITextField!
+    @IBOutlet weak var repeatPasswordText: UITextField!
+    @IBOutlet weak var saveButton: UIButton!
+    
+    private var passwordsMatch: Bool{
+        return newPasswordText.text == repeatPasswordText.text
     }
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Do any additional setup after loading the view.
+//    }
+    
     
 
     /*
@@ -26,5 +35,20 @@ class PasswordViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    @IBAction func touchSave(_ sender: UIButton) {
+        if passwordsMatch {
+            //TODO: set new password
+        } else {
+            let alert = UIAlertController(
+                title: "Fehler", message: "Passwörter stimmen nicht überein",
+                preferredStyle: .alert)
+            alert.addAction(
+                UIAlertAction(
+                    title: NSLocalizedString("OK", comment: "Default Action"),
+                    style: .default)
+            )
+            self.present(alert, animated: true, completion: nil)
+        }
+    }
+    
 }
