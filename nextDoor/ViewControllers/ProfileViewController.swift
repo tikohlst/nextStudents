7c7bb61f41efc209a9a4027d93a49946ac48c9e5
Benedict Zendel <benedict.zendel@student.hs-rm.de>
2020-06-13 14:05:15 +0200

    Add offer creation logic and ui

--- nextDoor/ViewControllers/ProfileViewController.swift
+++ nextDoor/ViewControllers/ProfileViewController.swift
@@ -164,7 +164,7 @@ class ProfileViewController: UIViewController, UITextViewDelegate, UITextFieldDe
                 "bio" : user.bio ?? ""
             ]) { err in
                 if let err = err {
-                    print("Error editing document: \(err)")
+                    print("Error editing document: \(err.localizedDescription)")
                 }
             }
             // profile image upload
--- nextDoor/ViewControllers/SettingsTableViewController.swift
+++ nextDoor/ViewControllers/SettingsTableViewController.swift
@@ -8,6 +8,7 @@
 import UIKit
 import Firebase
 import FirebaseFirestoreSwift
+import FirebaseStorage
 
 class SettingsTableViewController: UITableViewController {
 
