384851eb407d9464544678c4ad16eee72340a138
Benedict Zendel <benedict.zendel@student.hs-rm.de>
2020-06-14 13:00:02 +0200

    firebaseCLI setup

--- FirebaseCLI/functions/index.js
+++ FirebaseCLI/functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
-
+const admin = require('firebase-admin');
+admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
