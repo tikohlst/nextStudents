eb05905670ed4bb3a755da9c9313d2f0f93dbfe9
Tim Kohlstadt <info@tikohlst.de>
2020-06-21 21:49:33 +0200

    Update the UI representation of the view for updating the password

--- nextDoor/ViewControllers/PasswordViewController.swift
+++ nextDoor/ViewControllers/PasswordViewController.swift
@@ -5,58 +5,142 @@
 //  Copyright © 2020 Tim Kohlstadt, Benedict Zendel. All rights reserved.
 //
 
-import UIKit
+import Eureka
 
-class PasswordViewController: UIViewController, UITextFieldDelegate {
-    
-    @IBOutlet weak var newPasswordText: UITextField!
-    @IBOutlet weak var repeatPasswordText: UITextField!
-    @IBOutlet weak var saveButton: UIButton!
+public struct RuleMinLength: RuleType {
 
-    private var passwordsMatch: Bool{
-        return newPasswordText.text == repeatPasswordText.text
+    let min: UInt
+
+    public var id: String?
+    public var validationError: ValidationError
+
+    public init(minLength: UInt, msg: String? = nil, id: String? = nil) {
+        let ruleMsg = msg ?? "Das Passwort muss mindestens \(minLength) Zeichen enthalten"
+        min = minLength
+        validationError = ValidationError(msg: ruleMsg)
+        self.id = id
     }
+
+    public func isValid(value: String?) -> ValidationError? {
+        guard let value = value, !value.isEmpty else { return nil }
+        return value.count < Int(min) ? validationError : nil
+    }
+}
+
+public struct RuleMaxLength: RuleType {
+
+    let max: UInt
+
+    public var id: String?
+    public var validationError: ValidationError
+
+    public init(maxLength: UInt, msg: String? = nil, id: String? = nil) {
+        let ruleMsg = msg ?? "Das Passwort darf maximal \(maxLength) Zeichen enthalten"
+        max = maxLength
+        validationError = ValidationError(msg: ruleMsg)
+        self.id = id
+    }
+
+    public func isValid(value: String?) -> ValidationError? {
+        guard let value = value, !value.isEmpty else { return nil }
+        return value.count > Int(max) ? validationError : nil
+    }
+}
+
+public struct RuleEqualsToRow<T: Equatable>: RuleType {
+
+    public init(form: Form, tag: String, msg: String = "Die Passwörter stimmen nicht überein!", id: String? = nil) {
+        self.validationError = ValidationError(msg: msg)
+        self.form = form
+        self.tag = tag
+        self.row = nil
+        self.id = id
+    }
+
+    public init(row: RowOf<T>, msg: String = "Die Passwörter stimmen nicht überein!", id: String? = nil) {
+        self.validationError = ValidationError(msg: msg)
+        self.form = nil
+        self.tag = nil
+        self.row = row
+        self.id = id
+    }
+
+    public var id: String?
+    public var validationError: ValidationError
+    public weak var form: Form?
+    public var tag: String?
+    public weak var row: RowOf<T>?
+
+    public func isValid(value: T?) -> ValidationError? {
+        let rowAux: RowOf<T> = row ?? form!.rowBy(tag: tag!)!
+        return rowAux.value == value ? nil : validationError
+    }
+}
+
+class PasswordViewController: FormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
 
-        // Must be set for func textFieldShouldReturn()
-        newPasswordText.delegate = self
-        repeatPasswordText.delegate = self
-    }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-    @IBAction func touchSave(_ sender: UIButton) {
-        if passwordsMatch {
-            //TODO: set new password
-        } else {
-            let alert = UIAlertController(
-                title: "Fehler", message: "Passwörter stimmen nicht überein",
-                preferredStyle: .alert)
-            alert.addAction(
-                UIAlertAction(
-                    title: NSLocalizedString("OK", comment: "Default Action"),
-                    style: .default)
-            )
-            self.present(alert, animated: true, completion: nil)
+        // Design for LabelRow as validation error
+        LabelRow.defaultCellUpdate = { cell, row in
+            cell.contentView.backgroundColor = .red
+            cell.textLabel?.textColor = .white
+            cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 13)
+            cell.textLabel?.textAlignment = .right
         }
-    }
 
-    // This function is called when you click return key in the text field.
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        // Resign the first responder from textField to close the keyboard.
-        textField.resignFirstResponder()
-        return true
-    }
+        // Present label on the left side of the row in
+        // red if the value on the right side is invalid
+        PasswordRow.defaultCellUpdate = { cell, row in
+            if !row.isValid {
+                cell.titleLabel?.textColor = .red
+            }
+        }
 
+        // Add new LabelRow below the PasswordRow to display existing errors
+        PasswordRow.defaultOnRowValidationChanged = { cell, row in
+            let rowIndex = row.indexPath!.row
+            while row.section!.count > rowIndex + 1 && row.section?[rowIndex  + 1] is LabelRow {
+                row.section?.remove(at: rowIndex + 1)
+            }
+            if !row.isValid {
+                for (index, validationMsg) in row.validationErrors.map({ $0.msg }).enumerated() {
+                    let labelRow = LabelRow() {
+                        $0.title = validationMsg
+                        $0.cell.height = { 30 }
+                    }
+                    let indexPath = row.indexPath!.row + index + 1
+                    row.section?.insert(labelRow, at: indexPath)
+                }
+            }
+        }
+
+        form
+            +++ Section()
+
+            <<< PasswordRow("password") {
+                    $0.title = "Neues Password"
+                    $0.add(rule: RuleRequired(msg: "Du musst erst ein neues Passwort eingeben."))
+                    $0.add(rule: RuleMinLength(minLength: 8, msg:  "Das Passwort muss mindestens 8 Zeichen enthalten"))
+                    $0.add(rule: RuleMaxLength(maxLength: 16, msg: "Das Passwort darf maximal 16 Zeichen enthalten"))
+                }
+
+            <<< PasswordRow() {
+                    $0.title = "Password bestätigen"
+                    $0.add(rule: RuleEqualsToRow(form: form, tag: "password", msg: "Die Passwörter stimmen nicht überein"))
+                }
+
+            +++ Section()
+                <<< ButtonRow() {
+                    $0.title = "Passwort ändern"
+                }
+                .onCellSelection { cell, row in
+                    if row.section?.form?.validate().isEmpty ?? false {
+                        // TODO: Save the new password
+                        self.navigationController?.popViewController(animated: true)
+                        self.dismiss(animated: true, completion: nil)
+                    }
+                }
+    }
 }
