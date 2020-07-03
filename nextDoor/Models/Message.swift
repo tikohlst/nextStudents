c102b778a92eece92aabd0deec0af9b9f432ea0e
Tim Kohlstadt <info@tikohlst.de>
2020-07-03 22:10:14 +0200

    Remove a meaningless variable from the struct Message

--- nextDoor/Models/Message.swift
+++ nextDoor/Models/Message.swift
@@ -16,15 +16,13 @@ struct Message {
     var content: String
     var created: Timestamp
     var senderID: String
-    var senderName: String
 
     var dictionary: [String: Any] {
         return [
             "id": id,
             "content": content,
             "created": created,
-            "senderID": senderID,
-            "senderName": senderName ]
+            "senderID": senderID ]
     }
 
 }
@@ -37,11 +35,10 @@ extension Message {
         guard let id = dictionary["id"] as? String,
             let content = dictionary["content"] as? String,
             let created = dictionary["created"] as? Timestamp,
-            let senderID = dictionary["senderID"] as? String,
-            let senderName = dictionary["senderName"] as? String
+            let senderID = dictionary["senderID"] as? String
             else {return nil}
 
-        self.init(id: id, content: content, created: created, senderID: senderID, senderName:senderName)
+        self.init(id: id, content: content, created: created, senderID: senderID)
     }
 
 }
@@ -49,7 +46,7 @@ extension Message {
 extension Message: MessageType {
 
     var sender: SenderType {
-        return Sender(id: senderID, displayName: senderName)
+        return Sender(id: senderID, displayName: "")
     }
 
     var messageId: String {
