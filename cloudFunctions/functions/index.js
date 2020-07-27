aacf16344a067fc528d747967edc8895cede3284
Benedict Zendel <benedict.zendel@student.hs-rm.de>
2020-07-27 19:13:08 +0200

    Add offer deletion cloud function

--- cloudFunctions/functions/index.js
+++ cloudFunctions/functions/index.js
@@ -1,9 +1,38 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+var compare_dates = function(date1,date2){
+     if (date1>date2) return 1;
+   else if (date1<date2) return -1;
+   else return 0; 
+  }
+
+var add_minutes =  function (dt, minutes) {
+    return new Date(dt.getTime() + minutes*60000);
+}
 
  // Create and Deploy Your First Cloud Functions
  // https://firebase.google.com/docs/functions/write-firebase-functions
+  exports.deleteExpiredOffers = functions.region('europe-west1').https.onRequest(async (req, res) => {
+    const db = admin.firestore();
 
- exports.helloWorld = functions.https.onRequest((request, response) => {
-   functions.logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });
+    // get the offer collection of every users offers document
+    const snapshot = await db.collectionGroup('offer').get();
+    snapshot.forEach((doc) => {
+      const timeNow = Date.now();
+      const data = doc.data();
+      var factor = 1;
+      const timeFormat = data["timeFormat"]
+      if (timeFormat === 'Std.') {
+        factor = 60;
+      }
+      const expireDate = add_minutes(data["date"].toDate(), Number(data["duration"]) * factor);
+      if (compare_dates(timeNow, expireDate) === 1) {
+        console.log(data);
+        const ref = doc.ref;
+        doc.delete();
+      }
+    });
+    res.sendStatus(200);
+  })
